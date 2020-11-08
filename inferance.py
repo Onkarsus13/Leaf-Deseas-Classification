8318f59232ef73be8363ff1f18bc7366d1ee70f8
ONKAR Susladkar <46415298+Onkarsus13@users.noreply.github.com>
2020-11-08 15:01:27 +0530

    Add files via upload

---
+++ inferance.py
@@ -0,0 +1,52 @@
+import tensorflow.keras.backend as K
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras import regularizers
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, AveragePooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.regularizers import l2
+from tensorflow import keras
+from tensorflow.keras import models
+import cv2
+
+food_list = ['anthracnose','black_spot','phytophthora','powdery_mildew','ring_spot']
+model_best = load_model('papaya.hdf5',compile = False)
+images = []
+images.append('p9.jpg')
+
+
+
+def predict_class(model, images, show = True):
+  for img in images:
+    img = image.load_img(img, target_size=(64,64))
+    img = image.img_to_array(img)                    
+    img = np.expand_dims(img, axis=0)         
+    img /= 255.                                      
+
+    pred = model.predict(img)
+    index = np.argmax(pred)
+    print(index)
+    
+    print(food_list)
+    pred_value = food_list[index]
+    if show:
+        plt.imshow(img[0])                           
+        plt.axis('off')
+        plt.title(pred_value)
+        plt.show()
+
+
+if __name__ == '__main__':
+    predict_class(model_best , images , True)
+
