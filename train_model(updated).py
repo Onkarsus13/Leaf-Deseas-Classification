a79bf4aacc396e31db1140e37743b4a58fc2abbd
ONKAR Susladkar <46415298+Onkarsus13@users.noreply.github.com>
2020-11-08 15:17:03 +0530

    Add files via upload

---
+++ train_model(updated).py
@@ -0,0 +1,76 @@
+import tensorflow.keras.backend as K
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras import regularizers
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, AveragePooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.regularizers import l2
+from tensorflow import keras
+from tensorflow.keras import models
+import cv2
+import matplotlib.pyplot as plt
+
+
+
+n_classes = 5
+hight ,width = 32,32
+train_dir = 'Train'
+val_dir = 'Test'
+
+batch_size = 16
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(hight,width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+resnet = tf.keras.applications.ResNet50(weights = 'imagenet' , include_top = False , input_shape = (32, 32, 3))
+resnet.trainable = False
+x = resnet.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(128,activation='relu')(x)
+x = Dropout(0.2)(x)
+predictions = Dense(n_classes,kernel_regularizer=regularizers.l2(0.005), activation='softmax')(x)
+
+model = Model(inputs=resnet.input, outputs=predictions)
+model.compile(optimizer= 'SGD', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+history = model.fit_generator(train_generator,
+                    steps_per_epoch = 150,
+                    epochs=350,
+                    verbose=1,
+                    )
+model.save('papaya.hdf5')
+
+
+plt.title('epochs vs loss')
+plt.plot(history.history['loss'])
+
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train_loss', 'validation_loss'], loc='best')
+plt.show()
+
+
+
+
